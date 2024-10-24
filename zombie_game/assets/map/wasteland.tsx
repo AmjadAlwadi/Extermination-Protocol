ac7488f44e5c46114ef8b3af72368f197d361f8f
Amjad Alwadi <a.alwadi@tu-braunschweig.de>
2024-10-24 20:05:06 +0200

    adding all src files and assets

---
+++ zombie_game/assets/map/wasteland.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="wasteland" tilewidth="16" tileheight="16" tilecount="4392" columns="72">
+ <image source="wasteland.png" trans="402e19" width="1163" height="981"/>
+ <tile id="755">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="756">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="757">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="758">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="759">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="760">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="761">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="762">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="763">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="764">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="765">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1167">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1168">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1239">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1240">
+  <properties>
+   <property name="map_id" type="int" value="1"/>
+  </properties>
+ </tile>
+</tileset>
